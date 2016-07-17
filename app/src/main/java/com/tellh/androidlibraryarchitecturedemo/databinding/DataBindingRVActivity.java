e67defb2943192fe2fc6f9131c9b35d5ee685320
TellH <570495627@qq.com>
2016-07-17 09:58:26 +0800

    上传书的图片

--- app/src/main/java/com/tellh/androidlibraryarchitecturedemo/databinding/DataBindingRVActivity.java
+++ app/src/main/java/com/tellh/androidlibraryarchitecturedemo/databinding/DataBindingRVActivity.java
@@ -1,10 +1,15 @@
 package com.tellh.androidlibraryarchitecturedemo.databinding;
 
+import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.tellh.androidlibraryarchitecturedemo.R;
 
 import java.util.ArrayList;
@@ -34,11 +39,17 @@ public class DataBindingRVActivity extends AppCompatActivity {
         books.add(new Book("易中天中华史", "易中天", "浙江文艺出版社"));
         books.add(new Book("有味", "汪涵", "广西师范大学出版社"));
         books.add(new Book("风中的纸屑", "周国平", "浙江文艺出版社"));
+        books.add(new Book("MacBook", "Steve Job", "Apple Inc"));
         rv.setAdapter(new CommonAdapter(books) {
             @Override
             protected int getItemLayoutId(int viewType) {
                 return R.layout.item_rv_book;
             }
         });
+    }
+    @BindingAdapter({"imageUrl", "error"})
+    public static void loadImage(ImageView view, String url, Drawable error) {
+        Log.d("DataBindingRVActivity", "loadImage() called with: " + "view = [" + view + "], url = [" + url + "], error = [" + error + "]");
+        Picasso.with(view.getContext()).load(url).error(error).into(view);
     }
 }
