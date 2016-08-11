f8ef9f3ca2c52bea2e20331f68328e166cb17c13
TellH <570495627@qq.com>
2016-08-11 16:04:42 +0800

    Mockito

--- app/src/main/java/com/tellh/androidlibraryarchitecturedemo/dagger2/MvpPresenter.java
+++ app/src/main/java/com/tellh/androidlibraryarchitecturedemo/dagger2/MvpPresenter.java
@@ -9,9 +9,27 @@ import javax.inject.Inject;
  */
 public class MvpPresenter {
     MvpView mView;
+    private UserManager mUserManager;
 
     @Inject @PerActivity
     public MvpPresenter(MvpView mView) {
         this.mView = mView;
+    }
+
+    public void login(String name,String password){
+        if (name == null || name.length() == 0) return;
+        if (password == null || password.length() < 6) return;
+
+        mUserManager.performLogin(name, password);
+    }
+
+    public void setmUserManager(UserManager mUserManager) {
+        this.mUserManager = mUserManager;
+    }
+
+    public static class UserManager{
+        public void performLogin(String name, String password) {
+
+        }
     }
 }
